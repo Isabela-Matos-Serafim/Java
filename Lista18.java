f8912068ed34796fbe50e3fd90adcad2c0b8917f
Isabela-Matos-Serafim <isabelamatosserafim@gmail.com>
2025-10-03 16:18:19 -0300

    Add files via upload

---
+++ Lista18.java
@@ -0,0 +1,79 @@
+
+public class Lista18 {
+
+	public static void main(String[] args) {
+	
+		uno();
+		duno();
+		treno();
+		quano();
+		quino();
+		sesno();
+		seteno();
+		oiteno();
+		noveno();
+	}
+	public static void uno() {
+		for(int i= 0; i<7; i++) {
+			System.out.println("Isinha Gameplays");
+		}
+	}
+
+	public static void duno() {
+		System.out.println("Desista não persista");
+	}
+	
+	public static void treno() {
+		for(int i=1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void quano() {
+		System.out.println("A E I O U");
+	}
+	
+	public static void quino() {
+		System.out.println("****");
+		System.out.println("*  *");
+		System.out.println("****");
+	}
+	
+	public static void sesno() {
+		for(int i = 2; i <=20;i++) {
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	public static void seteno() {
+		for(int i=1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Fim");
+	}
+
+	public static void oiteno() {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+	}
+	
+	public static void noveno() {
+		System.out.println("Domingo");
+		System.out.println("Segunda");
+		System.out.println("Terça");
+		System.out.println("Quarta");
+		System.out.println("Quinta");
+		System.out.println("Sexta");
+		System.out.println("Sabado");
+	}
+	
+	
+	
+	
+	
+}
